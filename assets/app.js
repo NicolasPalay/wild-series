a4ac6ea86e0c6157627bdb610dbda7792a7eaeaa
unknown <nicolassam33@gmail.com>
2023-05-22 22:16:57 +0200

    relation program to category

--- assets/app.js
+++ assets/app.js
@@ -1,3 +1,4 @@
+
 /*
  * Welcome to your app's main JavaScript file!
  *
@@ -5,11 +6,11 @@
  * (and its CSS file) in your base layout (base.html.twig).
  */
 // assets/app.js
+// the bootstrap module doesn't export/return anything
 require('bootstrap');
-
 // any CSS you import will output into a single css file (app.scss in this case)
 import './styles/app.scss';
-import 'bootstrap';
+
 
 // start the Stimulus application
 import './bootstrap';
@@ -18,12 +19,3 @@ import './bootstrap';
 
 
 
-
-// this waits for Turbo Drive to load
-document.addEventListener('turbo:load', function (e) {
-    // this enables bootstrap tooltips globally
-    let tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-    let tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-        return new Tooltip(tooltipTriggerEl)
-    });
-});
---
+++ src/DataFixtures/CategoryFixtures.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Category;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class CategoryFixtures extends Fixture
+{
+    const CATEGORIES =['Action','Aventure','Animation','fantatique','Horreur'];
+
+    public function load(ObjectManager $manager): void
+    {
+
+
+        foreach (self::CATEGORIES as $key =>$categoryName){
+            $category = new Category();
+            $category->setName($categoryName);
+            $manager->persist($category);
+         }
+        $manager->flush();
+    }
+}
